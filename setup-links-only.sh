b913b7ab9a300c59e288491fcf60392e38173852
William Tayeb <williamtayeb@gmail.com>
2019-08-10 15:53:38 +0200

    Add function to ls after each cd command

--- setup-links-only.sh
+++ setup-links-only.sh
@@ -4,14 +4,16 @@ rm -rf $HOME/.vimrc
 rm -rf $HOME/.williamrc
 rm -rf $HOME/.gitignore
 rm -rf $HOME/.gitconfig
+rm -rf $HOME/.tmux.conf
 rm -rf $HOME/.config/nvim/init.vim
 
 ln -s $DOTFILES/.williamrc $HOME/.williamrc
 ln -s $DOTFILES/.vimrc $HOME/.vimrc
 ln -s $DOTFILES/.gitignore $HOME/.gitignore
 ln -s $DOTFILES/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILES/.tmux.conf $HOME/.tmux.conf
 
 mkdir -p $HOME/.config/nvim
 ln -s $DOTFILES/.config/nvim/init.vim $HOME/.config/nvim/init.vim
 
-echo "source ~/.williamrc" >> $HOME/.zshrc
+grep -qxF 'source ~/.williamrc' $HOME/.zshrc || echo 'source ~/.williamrc' >> $HOME/.zshrc
--- setup.sh
+++ setup.sh
@@ -101,6 +101,7 @@ rm -rf $HOME/.vimrc
 rm -rf $HOME/.williamrc
 rm -rf $HOME/.gitignore
 rm -rf $HOME/.gitconfig
+rm -rf $HOME/.tmux.conf
 rm -rf $HOME/.config/nvim/init.vim
 ok
 
@@ -109,12 +110,13 @@ ln -s $DOTFILES/.williamrc $HOME/.williamrc
 ln -s $DOTFILES/.vimrc $HOME/.vimrc
 ln -s $DOTFILES/.gitignore $HOME/.gitignore
 ln -s $DOTFILES/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILES/.tmux.conf $HOME/.tmux.conf
 
-mkdir -p $HOME/.config/nvim/init.vim
+mkdir -p $HOME/.config/nvim
 ln -s $DOTFILES/.config/nvim/init.vim $HOME/.config/nvim/init.vim
 ok
 
-echo "source ~/.williamrc" >> $HOME/.zshrc
+grep -qxF 'source ~/.williamrc' $HOME/.zshrc || echo 'source ~/.williamrc' >> $HOME/.zshrc
 
 echo
 bot "All done. Kill this terminal."
