98855700f19ba1958aa4bd1ced9a26a6b52e6130
William Tayeb <tayebwilliam@gmail.com>
2020-01-26 13:42:55 +0100

    Configure shiori

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,2 +1,5 @@
 test -d "${GOPATH}" || mkdir "${GOPATH}"
 test -d "${GOPATH}/src/github.com" || mkdir -p "${GOPATH}/src/github.com"
+
+# Install shiori bookmark manager
+go get -u -v github.com/go-shiori/shiori
