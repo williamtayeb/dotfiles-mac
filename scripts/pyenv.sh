a4eb29f7e8c13ca6b03010815e21898ebcbd8051
William Tayeb <tayebwilliam@gmail.com>
2020-01-30 19:12:05 +0100

    Configure global gitignore correctly

--- scripts/pyenv.sh
+++ scripts/pyenv.sh
@@ -1,3 +1,6 @@
+# Required in order to install YouCompleteMe
+export PYTHON_CONFIGURE_OPTS="--enable-framework"
+
 # Install latest pyenv python
 pyenv install $(pyenv install --list | sed 's/^  //' | grep '^\d' | grep --invert-match 'dev\|a\|b' | tail -1)
 
